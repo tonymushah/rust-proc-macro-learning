751540c06ddcaa69c44fdd1008cffd6ba7885cd2
tonymushah <tonyramanantsalama@gmail.com>
2024-06-23 21:23:05 +0300

    Updated main for `log_duration` test

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,16 @@
-use std::collections::HashMap;
+use rpml_macros::log_duration;
 
-use rpml_macros::{DeriveCustomModel, IntoStringHashMap};
+#[log_duration]
+#[must_use]
+fn function_to_benchmark() -> u16 {
+    let mut counter = 0;
+    for _ in 0..u16::MAX {
+        counter += 1;
+    }
 
-#[derive(DeriveCustomModel)]
-#[custom_model(model(
-    name = "UserName",
-    fields(first_name, last_name),
-    extra_derives(IntoStringHashMap)
-))]
-#[custom_model(model(name = "UserInfo", fields(username, age), extra_derives(Debug)))]
-pub struct User2 {
-    username: String,
-    first_name: String,
-    last_name: String,
-    age: u32,
+    counter
 }
 
 fn main() {
-    let user_name = UserName {
-        first_name: "first_name".to_string(),
-        last_name: "last_name".to_string(),
-    };
-    let hash_map = HashMap::<String, String>::from(user_name);
-
-    dbg!(hash_map);
-
-    let user_info = UserInfo {
-        username: "username".to_string(),
-        age: 27,
-    };
-
-    dbg!(user_info);
+    println!("{}", function_to_benchmark());
 }
