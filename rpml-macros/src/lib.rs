1660876b75ed9f860b83c3e3b7e876487e04985b
tonymushah <tonyramanantsalama@gmail.com>
2024-06-23 10:29:04 +0300

    implemented `IntoStringHashMap` derive

--- rpml-macros/src/lib.rs
+++ rpml-macros/src/lib.rs
@@ -1,6 +1,33 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput};
 
 #[proc_macro_derive(IntoStringHashMap)]
 pub fn derive_into_hashmap(item: TokenStream) -> TokenStream {
-    todo!()
+    let input = parse_macro_input!(item as DeriveInput);
+    let struct_indentifier = &input.ident;
+    match &input.data {
+        Data::Struct(DataStruct{ fields, .. }) => {
+            let mut implementation = quote! {
+                let mut hash_map = std::collections::HashMap::<String, String>::new();
+            };
+            for field in fields {
+                let identifier = field.ident.as_ref().unwrap();
+                implementation.extend(quote! {
+                    hash_map.insert(stringify!(#identifier).to_string(), String::from(value.#identifier));
+                })
+            }
+            quote!{
+                #[automatically_derived]
+                impl From<#struct_indentifier> for std::collections::HashMap<String, String> {
+                    fn from(value: #struct_indentifier) -> Self {
+                        #implementation
+
+                        hash_map
+                    }
+                }
+            }
+        },
+        _ => unimplemented!()
+    }.into()
 }
