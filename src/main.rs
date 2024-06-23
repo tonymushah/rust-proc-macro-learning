cdc866e772d52170b737c785cba3d73d3e7be8b6
tonymushah <tonyramanantsalama@gmail.com>
2024-06-23 09:46:55 +0300

    Added `User` test struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rpml_macros::IntoStringHashMap;
+
+#[derive(Debug, IntoStringHashMap)]
+struct User {
+    username: String,
+    first_name: String,
+    last_name: String,
+    age: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
